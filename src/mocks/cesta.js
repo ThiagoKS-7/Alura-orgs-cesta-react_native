80e77f03a06197bd167eddecf5132dc353ecca69
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-05-10 18:57:13 -0300

    feat(#2): refatorações para componentizar o app e separar melhor cada coisa

---
+++ src/mocks/cesta.js
@@ -0,0 +1,17 @@
+import topo from '../../assets/topo.png'
+import logo from '../../assets/logo.png'
+const cesta = {
+    topo: {
+        titulo: "Detalhe da cesta",
+        imagem:topo,
+    },
+    detalhes: {
+        nome: "Cesta de verduras",
+        imagem:logo,
+        nomeFazenda: "Jenny Jack Farm",
+        descricao:"Uma cesta com produtos selecionados cuidadosamente  da fazenda direto para a sua cozinha",
+        preco:"R$ 40,00",
+    }  
+}
+
+export default cesta;
